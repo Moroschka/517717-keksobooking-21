0bf3645c24259e09347dc1f4512de8b8ea048ca1
Moroschka <leda79@rambler.ru>
2020-10-03 21:57:55 +0300

    Внесены правки

--- js/main.js
+++ js/main.js
@@ -8,7 +8,22 @@ const PHOTOS = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0
 const TYPE_HOUSING = [`palace`, `flat`, `house`, `bungalow`];
 const PIN_WIDTH = 50;
 const PIN_HEIGHT = 70;
-
+const price = {
+  min: 5000,
+  max: 10000
+};
+const rooms = {
+  min: 1,
+  max: 5
+};
+const guests = {
+  min: 1,
+  max: 10
+};
+const rangeY = {
+  min: 130,
+  max: 630
+};
 const pinOfferTemplate = document.querySelector(`#pin`).content;
 const fieldMap = document.querySelector(`.map`);
 const similarListElement = fieldMap.querySelector(`.map__pins`);
@@ -25,25 +40,24 @@ makeMapActive();
 
 const createOffersMock = function () {
   const createOffer = function (index) {
-    const locationX = getRandomNumber(PIN_WIDTH, mapWidth - PIN_WIDTH);
-    const locationY = getRandomNumber(130, 630);
-
+    const locationX = getRandomNumber(0, mapWidth);
+    const locationY = getRandomNumber(rangeY.min, rangeY.max);
     return {
       "author": {
-        "avatar": `img/avatars/user` + (index < 10 ? `0${index}` : index) + `.png`
+        "avatar": `img/avatars/user${(index < 10 ? `0${index}` : index)}.png`
       },
       "offer": {
         "title": `Заголовок ${index}`,
         "address": `${locationX}, ${locationY}`,
-        "price": getRandomNumber(5000, 100000),
+        "price": getRandomNumber(price.min, price.max),
         "type": TYPE_HOUSING[getRandomNumber(0, TYPE_HOUSING.length)],
-        "rooms": getRandomNumber(1, 5),
-        "guests": getRandomNumber(1, 10),
+        "rooms": getRandomNumber(rooms.min, rooms.max),
+        "guests": getRandomNumber(guests.min, guests.max),
         "checkin": CHECKIN[getRandomNumber(0, CHECKIN.length)],
         "checkout": CHECKOUT[getRandomNumber(0, CHECKOUT.length)],
-        "features": FEATURES.slice(0, getRandomNumber(0, FEATURES.length + 1)),
+        "features": FEATURES.filter(() => Math.random() < 0.5),
         "description": `Описание ${index}`,
-        "photos": PHOTOS[getRandomNumber(0, PHOTOS.length)]
+        "photos": PHOTOS.filter(() => Math.random() < 0.5)
       },
       "location": {
         "x": locationX,
@@ -53,7 +67,7 @@ const createOffersMock = function () {
   };
 
   let offersMock = [];
-  for (let i = 1; i < NUMBER_OFFERS + 1; i++) {
+  for (let i = 1; i < NUMBER_OFFERS; i++) {
     offersMock.push(createOffer(i));
   }
   return offersMock;
@@ -68,6 +82,7 @@ const renderPinOffer = function (offer) {
   pinOfferElement.querySelector(`.map__pin img`).setAttribute(`alt`, offer.offer.title);
 
   return pinOfferElement;
+
 };
 
 const fillBlockOffer = function () {
